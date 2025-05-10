89b299637ee2bac654c4078cdb896a7118e5cd0c
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-10 14:43:11 -0400

    Update connect.js

--- storyapp1/api/routes/connect.js
+++ storyapp1/api/routes/connect.js
@@ -1,8 +1,9 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"djt23actor",
-  database:"storyapp"
+  host: process.env.DB_HOST,       // e.g. 'your-db-host.aivencloud.com'
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // adjust if needed
 })
