476406d99ab1d55e346134acecf87da2a2bc27f3
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-11 00:29:58 -0400

    Update axios.js

--- storyapp1/comcreates/src/axios.js
+++ storyapp1/comcreates/src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const PORT = process.env.PORT || 8800;
 
 export const makeRequest = axios.create({
-  baseURL: `https://z-creates-production.up.railway.app/${PORT}/api/`,
+  baseURL: "https://z-creates-production.up.railway.app/api/",
   withCredentials: true,
 });
