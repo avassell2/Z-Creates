471be3572978cdbd189fe47d1e4aa44079435da5
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-11 05:31:01 -0400

    Update index.js

--- storyapp1/api/index.js
+++ storyapp1/api/index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 3000;
 //middlewares
 app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Credentials", true);
+    res.header( "Access-Control-Allow-Origin": "https://z-creates-yteg.onrender.com");
     next();
 });
 app.use(express.json());
