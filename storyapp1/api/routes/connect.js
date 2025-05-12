d8ffb24907b6c0a54ed923000b0a698977f647da
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-12 04:13:26 -0400

    Update connect.js

--- storyapp1/api/routes/connect.js
+++ storyapp1/api/routes/connect.js
@@ -6,7 +6,7 @@ export const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT || 3306,
 });
 
