2036b3ff2ce71f935b0a6d1233e3a8be393b9a5c
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-12 01:26:48 -0400

    Update connect.js

--- storyapp1/api/routes/connect.js
+++ storyapp1/api/routes/connect.js
@@ -1,7 +1,7 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-  //connectionLimit: 10,
+  connectionLimit: 10,
   host: process.env.MYSQLHOST,       
   user: process.env.MYSQLUSER,
   password: process.env.MYSQL_ROOT_PASSWORD,
@@ -9,11 +9,11 @@ export const db = mysql.createConnection({
   port: process.env.MYSQLPORT || 3306, // adjust if needed
 })
 
-//db.getConnection((err, connection) => {
- // if (err) {
-  //  console.error("DB connection failed:", err);
- // } else {
-  //  console.log("DB connected");
- //   connection.release(); // release back to pool
- // }
-//});
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("DB connection failed:", err);
+  } else {
+    console.log("DB connected");
+    connection.release(); // release back to pool
+  }
+});
