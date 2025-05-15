1f8bd95b4e73071fafa85149b21c020751ffa6dd
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-15 11:59:44 -0400

    Update Profile.js

--- storyapp1/comcreates/src/components/pages/profile/Profile.js
+++ storyapp1/comcreates/src/components/pages/profile/Profile.js
@@ -56,7 +56,7 @@ const getImagePath = (url) => {
       <div className="images">
         
       <img src={getImagePath(data?.coverPic)} alt="Cover" className="cover" />
-      <img src= "https://res.cloudinary.com/dmvlhxlpe/image/upload/v1747322448/no_image_gb87q1.png" alt="Profile" className="profilePic" />
+      <img src= {"https://res.cloudinary.com/dmvlhxlpe/image/upload/v1747322448/no_image_gb87q1.png"} alt="Profile" className="profilePic" />
 
       </div>
       <div className="profileContainer">
