7cb38aa1c6ff9670e732b540b16410d4fe8f117e
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-15 12:44:34 -0400

    Update UpdateComDetails.js

--- storyapp1/comcreates/src/components/update/UpdateComDetails.js
+++ storyapp1/comcreates/src/components/update/UpdateComDetails.js
@@ -50,7 +50,7 @@ const UpdateComDetails = ({setOpenUpdate, series}) => {
     const handleClick = async (e) => {
       e.preventDefault();
     if (!thumbnail) return alert("Please select an image file");
-    if (thumbnail && !(thumbnail.type && file.type.startsWith('image/'))) return alert("Please select an image file"); //stop user from uploading non-images
+    if (thumbnail && !(thumbnail.type && thumbnail.type.startsWith('image/'))) return alert("Please select an image file"); //stop user from uploading non-images
   
       //TODO: find a better way to get image URL
       
