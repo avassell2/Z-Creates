dd518946ee857086b0c7ead1fe1c291a831dffe1
avassell2 <55057511+avassell2@users.noreply.github.com>
2025-05-15 15:54:15 -0400

    Update Profile.js

--- storyapp1/comcreates/src/components/pages/profile/Profile.js
+++ storyapp1/comcreates/src/components/pages/profile/Profile.js
@@ -51,7 +51,7 @@ const {data ,error} =  useQuery({
   }
 
   // Fallback to local (for legacy support)
-  return https://z-creates-production.up.railway.app/upload/${url};
+  return `https://z-creates-production.up.railway.app/upload/${url}`;
 };
 
 
